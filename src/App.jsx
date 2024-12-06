25ecefa4b9bb6b9e60e4e27627584b223821de0a
Nick <NickolasKavanagh@proton.me>
2024-12-05 22:15:43 -0330

    Hero section started, fun motion added in app and hero

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,15 @@ import Contact from './components/Contact';
 function App() {
   return (
     <div className="fixed -z-10 min-h-screen w-full bg-slate-900">
-      <motion.div 
+      <motion.div
         className="fixed -z-10 min-h-screen w-full bg-[linear-gradient(to_right,#87CEEB33_1px,transparent_1px),linear-gradient(to_bottom,#87CEEB15_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"
         animate={{
-          x: [-14, 0],
+          y: [24, 0],
         }}
         transition={{
           repeat: Infinity,
-          duration: 0.5,
-          ease: "linear"
+          duration: 1,
+          ease: "linear",
         }}
       />
       <main className="flex flex-col items-center px-4 md:px-8 lg:px-16">
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,49 @@
+import { motion } from "framer-motion";
+import profilePicture from "../assets/profilePicture.jpg";
+
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div
+      id="home"
+      className="px-16 flex min-h-screen w-full items-center justify-center py-28 md:px-32"
+    >
+      <div className="flex flex-col items-center justify-center gap-10 text-white">
+        <motion.div
+          initial={{ opacity: 0, y: -100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.66, delay: 0.2 }}
+        >
+          <img
+            src={profilePicture}
+            alt=""
+            className="w-[150px] h-[150px] cursor-pointer rounded-full shadow-xl hover:-translate-y-5 hover:scale-105 hover:shadow-2xl transition-all duration-300 md:w-[250px] md:h-[250px] object-cover mt-10"
+          />
+        </motion.div>
 
-export default Hero
+        <motion.div
+          initial={{ opacity: 0, x: -100 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.66, delay: 0.2 }}
+          className="flex max-w-[600px] flex-col items-center justify-center gap-3 text-center"
+        >
+          <h1 className="text-4xl opacity-80">Hi, I'm Nick.</h1>
+          <motion.div
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.66, delay: 0.2 }}
+            className="flex max-w-[600px] flex-col items-center justify-center gap-3 text-center"
+          >
+            <p className="text-lg opacity-60">
+              I'm a student software developer and audio engineer with a passion
+              for audio interfacing software. I am experienced in various
+              technologies such as Python, React, Tailwind, and Motion as well as
+              audio technologies such as FL Studio, Web Audio API, and WebRTC.
+            </p>
+          </motion.div>
+        </motion.div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
