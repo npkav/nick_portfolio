5f6e5f08f68de531fb394ba7711e81120bac56af
Nick <NickolasKavanagh@proton.me>
2024-12-09 15:35:57 -0330

    technologies reactivity

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -10,39 +10,42 @@ import { SiTrello } from "react-icons/si";
 
 const Technologies = () => {
   return (
-    <div id="technologies" className="flex min-h-[70vh] w-full max-w-[800px] mx-auto flex-col items-center justify-center gap-16 md:gap-32 text-white">
+    <div
+      id="technologies"
+      className="flex min-h-[70vh] w-full max-w-[800px] mx-auto flex-col items-center justify-center gap-8 md:gap-16 text-white"
+    >
       <h1 className="text-4xl opacity-80">Technologies</h1>
       <div className="flex flex-wrap items-center justify-center gap-10 p-5">
-      <div>
-          <BiLogoPython className="cursor-pointer text-[100px] opacity-80 hover:opacity-100 hover:scale-110 transition-all duration-300"/>
+        <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 hover:opacity-100 hover:scale-110 transition-all duration-300">
+          <BiLogoPython />
         </div>
-        <div>
-          <SiJavascript className="cursor-pointer text-[100px] opacity-80 hover:opacity-100 hover:scale-110 transition-all duration-300"/>
+        <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 hover:opacity-100 hover:scale-110 transition-all duration-300">
+          <SiJavascript />
         </div>
-        <div>
-          <SiReact className="cursor-pointer text-[100px] opacity-80 hover:opacity-100 hover:scale-110 transition-all duration-300"/>
+        <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 hover:opacity-100 hover:scale-110 transition-all duration-300">
+          <SiReact />
         </div>
-        <div>
-          <SiTailwindcss className="cursor-pointer text-[100px] opacity-80 hover:opacity-100 hover:scale-110 transition-all duration-300"/>
+        <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 hover:opacity-100 hover:scale-110 transition-all duration-300">
+          <SiTailwindcss />
         </div>
-        <div>
-          <SiBootstrap className="cursor-pointer text-[100px] opacity-80 hover:opacity-100 hover:scale-110 transition-all duration-300"/>
+        <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 hover:opacity-100 hover:scale-110 transition-all duration-300">
+          <SiBootstrap />
         </div>
-        <div>
-          <SiFramer className="cursor-pointer text-[100px] opacity-80 hover:opacity-100 hover:scale-110 transition-all duration-300"/>
+        <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 hover:opacity-100 hover:scale-110 transition-all duration-300">
+          <SiFramer />
         </div>
-        <div>
-          <SiFigma className="cursor-pointer text-[100px] opacity-80 hover:opacity-100 hover:scale-110 transition-all duration-300"/>
+        <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 hover:opacity-100 hover:scale-110 transition-all duration-300">
+          <SiFigma />
         </div>
-        <div>
-          <SiWebrtc className="cursor-pointer text-[100px] opacity-80 hover:opacity-100 hover:scale-110 transition-all duration-300"/>
+        <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 hover:opacity-100 hover:scale-110 transition-all duration-300">
+          <SiWebrtc />
         </div>
-        <div>
-          <SiTrello className="cursor-pointer text-[100px] opacity-80 hover:opacity-100 hover:scale-110 transition-all duration-300"/>
+        <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 hover:opacity-100 hover:scale-110 transition-all duration-300">
+          <SiTrello />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Technologies 
+export default Technologies;
