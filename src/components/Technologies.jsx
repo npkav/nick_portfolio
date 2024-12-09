87eb8ac3150d1098ddb99ef1ec724d6a2fffc3c9
Nick <NickolasKavanagh@proton.me>
2024-12-09 17:13:10 -0330

    hover text under technologies

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -17,35 +17,65 @@ const Technologies = () => {
     >
       <h1 className="text-4xl opacity-80">Technologies</h1>
       <div className="flex flex-wrap items-center justify-center gap-10 p-5">
-        <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 hover:opacity-100 hover:scale-110 transition-all duration-300">
-          <BiLogoPython />
+        <div className="flex flex-col items-center group">
+          <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 group-hover:opacity-100 group-hover:scale-110 transition-all duration-300">
+            <BiLogoPython />
+          </div>
+          <span className="opacity-0 group-hover:opacity-100 transition-all duration-300 text-sm mt-2">Python</span>
         </div>
-        <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 hover:opacity-100 hover:scale-110 transition-all duration-300">
-          <SiJavascript />
+        <div className="flex flex-col items-center group">
+          <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 group-hover:opacity-100 group-hover:scale-110 transition-all duration-300">
+            <SiJavascript />
+          </div>
+          <span className="opacity-0 group-hover:opacity-100 transition-all duration-300 text-sm mt-2">JavaScript</span>
         </div>
-        <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 hover:opacity-100 hover:scale-110 transition-all duration-300">
-          <SiReact />
+        <div className="flex flex-col items-center group">
+          <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 group-hover:opacity-100 group-hover:scale-110 transition-all duration-300">
+            <SiReact />
+          </div>
+          <span className="opacity-0 group-hover:opacity-100 transition-all duration-300 text-sm mt-2">React</span>
         </div>
-        <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 hover:opacity-100 hover:scale-110 transition-all duration-300">
-          <SiTailwindcss />
+        <div className="flex flex-col items-center group">
+          <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 group-hover:opacity-100 group-hover:scale-110 transition-all duration-300">
+            <SiTailwindcss />
+          </div>
+          <span className="opacity-0 group-hover:opacity-100 transition-all duration-300 text-sm mt-2">Tailwind CSS</span>
         </div>
-        <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 hover:opacity-100 hover:scale-110 transition-all duration-300">
-          <SiBootstrap />
+        <div className="flex flex-col items-center group">
+          <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 group-hover:opacity-100 group-hover:scale-110 transition-all duration-300">
+            <SiBootstrap />
+          </div>
+          <span className="opacity-0 group-hover:opacity-100 transition-all duration-300 text-sm mt-2">Bootstrap</span>
         </div>
-        <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 hover:opacity-100 hover:scale-110 transition-all duration-300">
-          <SiFramer />
+        <div className="flex flex-col items-center group">
+          <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 group-hover:opacity-100 group-hover:scale-110 transition-all duration-300">
+            <SiWebrtc />
+          </div>
+          <span className="opacity-0 group-hover:opacity-100 transition-all duration-300 text-sm mt-2">WebRTC</span>
         </div>
-        <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 hover:opacity-100 hover:scale-110 transition-all duration-300">
-          <SiFigma />
+        <div className="flex flex-col items-center group">
+          <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 group-hover:opacity-100 group-hover:scale-110 transition-all duration-300">
+            <SiFramer />
+          </div>
+          <span className="opacity-0 group-hover:opacity-100 transition-all duration-300 text-sm mt-2">Framer</span>
         </div>
-        <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 hover:opacity-100 hover:scale-110 transition-all duration-300">
-          <SiWebrtc />
+        <div className="flex flex-col items-center group">
+          <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 group-hover:opacity-100 group-hover:scale-110 transition-all duration-300">
+            <SiFigma />
+          </div>
+          <span className="opacity-0 group-hover:opacity-100 transition-all duration-300 text-sm mt-2">Figma</span>
         </div>
-        <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 hover:opacity-100 hover:scale-110 transition-all duration-300">
-          <SiTrello />
+        <div className="flex flex-col items-center group">
+          <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 group-hover:opacity-100 group-hover:scale-110 transition-all duration-300">
+            <SiTrello />
+          </div>
+          <span className="opacity-0 group-hover:opacity-100 transition-all duration-300 text-sm mt-2">Trello</span>
         </div>
-        <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 hover:opacity-100 hover:scale-110 transition-all duration-300">
-          <SiAffinityphoto />
+        <div className="flex flex-col items-center group">
+          <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 group-hover:opacity-100 group-hover:scale-110 transition-all duration-300">
+            <SiAffinityphoto />
+          </div>
+          <span className="opacity-0 group-hover:opacity-100 transition-all duration-300 text-sm mt-2">Affinity Photo</span>
         </div>
       </div>
     </div>
