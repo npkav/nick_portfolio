24fe6dc19e1e651a509adbf95a13aeaee85434cd
Nick <NickolasKavanagh@proton.me>
2024-12-11 15:07:37 -0330

    redid technologies using map for future additions

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -9,6 +9,60 @@ import { SiWebrtc } from "react-icons/si";
 import { SiTrello } from "react-icons/si";
 import { SiAffinityphoto } from "react-icons/si";
 
+const technologiesData = [
+  {
+    icon: BiLogoPython,
+    name: "Python"
+  },
+  {
+    icon: SiJavascript,
+    name: "JavaScript"
+  },
+  {
+    icon: SiReact,
+    name: "React"
+  },
+  {
+    icon: SiTailwindcss,
+    name: "Tailwind CSS"
+  },
+  {
+    icon: SiBootstrap,
+    name: "Bootstrap"
+  },
+  {
+    icon: SiWebrtc,
+    name: "WebRTC"
+  },
+  {
+    icon: SiGit,
+    name: "Git"
+  },
+  {
+    icon: SiFigma,
+    name: "Figma"
+  },
+  {
+    icon: SiTrello,
+    name: "Trello"
+  },
+  {
+    icon: SiAffinityphoto,
+    name: "Affinity Photo"
+  }
+];
+
+const TechnologyCard = ({ Icon, name }) => (
+  <div className="flex flex-col items-center group">
+    <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 group-hover:opacity-100 group-hover:scale-110 transition-all duration-300">
+      <Icon />
+    </div>
+    <span className="opacity-0 group-hover:opacity-100 transition-all duration-300 text-sm mt-2">
+      {name}
+    </span>
+  </div>
+);
+
 const Technologies = () => {
   return (
     <div
@@ -17,95 +71,9 @@ const Technologies = () => {
     >
       <h1 className="text-4xl opacity-80">Technologies</h1>
       <div className="flex flex-wrap items-center justify-center gap-12 p-5">
-        <div className="flex flex-col items-center group">
-          <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 group-hover:opacity-100 group-hover:scale-110 transition-all duration-300">
-            <BiLogoPython />
-          </div>
-          <span className="opacity-0 group-hover:opacity-100 transition-all duration-300 text-sm mt-2">
-            Python
-          </span>
-        </div>
-
-        <div className="flex flex-col items-center group">
-          <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 group-hover:opacity-100 group-hover:scale-110 transition-all duration-300">
-            <SiJavascript />
-          </div>
-          <span className="opacity-0 group-hover:opacity-100 transition-all duration-300 text-sm mt-2">
-            JavaScript
-          </span>
-        </div>
-
-        <div className="flex flex-col items-center group">
-          <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 group-hover:opacity-100 group-hover:scale-110 transition-all duration-300">
-            <SiReact />
-          </div>
-          <span className="opacity-0 group-hover:opacity-100 transition-all duration-300 text-sm mt-2">
-            React
-          </span>
-        </div>
-
-        <div className="flex flex-col items-center group">
-          <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 group-hover:opacity-100 group-hover:scale-110 transition-all duration-300">
-            <SiTailwindcss />
-          </div>
-          <span className="opacity-0 group-hover:opacity-100 transition-all duration-300 text-sm mt-2">
-            Tailwind CSS
-          </span>
-        </div>
-
-        <div className="flex flex-col items-center group">
-          <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 group-hover:opacity-100 group-hover:scale-110 transition-all duration-300">
-            <SiBootstrap />
-          </div>
-          <span className="opacity-0 group-hover:opacity-100 transition-all duration-300 text-sm mt-2">
-            Bootstrap
-          </span>
-        </div>
-
-        <div className="flex flex-col items-center group">
-          <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 group-hover:opacity-100 group-hover:scale-110 transition-all duration-300">
-            <SiWebrtc />
-          </div>
-          <span className="opacity-0 group-hover:opacity-100 transition-all duration-300 text-sm mt-2">
-            WebRTC
-          </span>
-        </div>
-
-        <div className="flex flex-col items-center group">
-          <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 group-hover:opacity-100 group-hover:scale-110 transition-all duration-300">
-            <SiGit />
-          </div>
-          <span className="opacity-0 group-hover:opacity-100 transition-all duration-300 text-sm mt-2">
-            Git
-          </span>
-        </div>
-
-        <div className="flex flex-col items-center group">
-          <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 group-hover:opacity-100 group-hover:scale-110 transition-all duration-300">
-            <SiFigma />
-          </div>
-          <span className="opacity-0 group-hover:opacity-100 transition-all duration-300 text-sm mt-2">
-            Figma
-          </span>
-        </div>
-
-        <div className="flex flex-col items-center group">
-          <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 group-hover:opacity-100 group-hover:scale-110 transition-all duration-300">
-            <SiTrello />
-          </div>
-          <span className="opacity-0 group-hover:opacity-100 transition-all duration-300 text-sm mt-2">
-            Trello
-          </span>
-        </div>
-
-        <div className="flex flex-col items-center group">
-          <div className="text-[50px] sm:text-[75px] md:text-[100px] cursor-pointer opacity-60 group-hover:opacity-100 group-hover:scale-110 transition-all duration-300">
-            <SiAffinityphoto />
-          </div>
-          <span className="opacity-0 group-hover:opacity-100 transition-all duration-300 text-sm mt-2">
-            Affinity Photo
-          </span>
-        </div>
+        {technologiesData.map((tech, index) => (
+          <TechnologyCard key={index} Icon={tech.icon} name={tech.name} />
+        ))}
       </div>
     </div>
   );
