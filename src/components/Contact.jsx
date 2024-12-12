345c9b200ff43151e0ed518c27e3ff7a57e7a880
Nick <NickolasKavanagh@proton.me>
2024-12-11 22:06:46 -0330

    basic contact template

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,15 @@
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className="flex flex-col items-center justify-center gap-4 p-36 text-white">
+      <motion className="div"></motion>
+      <h1 className="text-4xl opacity-80">Send me an email!</h1>
+      <form className="flex flex-col items-right justify-center gap-4 bg-transparent p-10 rounded-lg">
+        <input type="text" placeholder="Name" className="bg-transparent border-2 border-white p-2 rounded-lg" />
+        <input type="email" placeholder="Email" className="bg-transparent border-2 border-white p-2 rounded-lg" />
+        <textarea placeholder="Message" className="bg-transparent border-2 border-white p-2 rounded-lg" />
+        <button type="submit" className="bg-transparent border-2 border-white p-2 rounded-lg">Send</button> 
+    </form>
+    </div>
   )
 }
 
