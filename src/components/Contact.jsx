a5f6bf6bba46a3a455e9654105ad77049a480f64
Nick <NickolasKavanagh@proton.me>
2024-12-13 13:12:04 -0330

    contact anims

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,14 +1,55 @@
+import { motion } from "framer-motion";
+
 const Contact = () => {
   return (
-    <div className="flex flex-col items-center justify-center gap-4 p-36 text-white">
-      <motion className="div"></motion>
-      <h1 className="text-4xl opacity-80">Send me an email!</h1>
-      <form className="flex flex-col items-right justify-center gap-4 bg-transparent p-10 rounded-lg">
-        <input type="text" placeholder="Name" className="bg-transparent border-2 border-white p-2 rounded-lg" />
-        <input type="email" placeholder="Email" className="bg-transparent border-2 border-white p-2 rounded-lg" />
-        <textarea placeholder="Message" className="bg-transparent border-2 border-white p-2 rounded-lg" />
-        <button type="submit" className="bg-transparent border-2 border-white p-2 rounded-lg">Send</button> 
-    </form>
+    <div id="contact" className="flex min-h-[100vh] w-full max-w-[1400px] mx-auto flex-col items-center justify-center gap-4 md:gap-8 text-white pt-36">
+      <motion.h1 
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 0.8 }}
+        transition={{ duration: 2 }}
+        className="text-4xl opacity-80"
+      >
+        Send me an email!
+      </motion.h1>
+      <motion.form 
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1, delay: 0.5 }}
+        className="flex flex-col items-right justify-center gap-4 bg-transparent p-10 rounded-lg"
+      >
+        <motion.input 
+          initial={{ opacity: 0, x: -20 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5, delay: 0.8 }}
+          type="text" 
+          placeholder="Name" 
+          className="bg-transparent border-2 border-white p-2 rounded-lg w-[400px]" 
+        />
+        <motion.input 
+          initial={{ opacity: 0, x: -20 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5, delay: 1.1 }}
+          type="email" 
+          placeholder="Email" 
+          className="bg-transparent border-2 border-white p-2 rounded-lg w-[400px]" 
+        />
+        <motion.textarea 
+          initial={{ opacity: 0, x: -20 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5, delay: 1.4 }}
+          placeholder="Message" 
+          className="bg-transparent border-2 border-white p-2 rounded-lg w-[400px]" 
+        />
+        <motion.button 
+          initial={{ opacity: 0, x: -20 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 0.5, delay: 1.7 }}
+          type="submit" 
+          className="bg-transparent border-2 border-white p-2 rounded-lg w-[400px]"
+        >
+          Send
+        </motion.button> 
+      </motion.form>
     </div>
   )
 }
